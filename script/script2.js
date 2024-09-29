aedec97e4de0e3e5bcdd54db4bb1723444738982
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-09-29 11:19:44 +0200

    editar

--- script/script2.js
+++ script/script2.js
@@ -1,6 +1,13 @@
 const button = document.querySelector('#todo-form button');
 const input = document.querySelector('#todo-form input');
+const buttonEdit = document.querySelector('#edit-form button');
+const inputEdit = document.querySelector('#edit-form input');
 const toDoListContainer = document.getElementById('todo-list');
+
+const editForm = document.querySelector("#edit-form");
+const todoForm = document.querySelector("#todo-form");
+const toolbar = document.querySelector("#toolbar");
+const cancelBtn = document.querySelector("#cancel-edit-btn");
 
 let myList = [];
 
@@ -17,7 +24,7 @@ function showToDo(){
                <div class="todo ${item.done && "done"}">
                   <h3>${item.text}</h3>
                   <button class="finish-todo" onclick="finishToDo(${index})"><i class="fa-solid fa-check"></i></button>
-                  <button class="edit-todo"><i class="fa-solid fa-pen"></i></button>
+                  <button class="edit-todo" onclick="editToggle(${index})"><i class="fa-solid fa-pen"></i></button>
                   <button class="remove-todo" onclick="deleteToDo(${index})"><i class="fa-solid fa-xmark"></i></button>
                </div>
             `
@@ -32,17 +39,43 @@ function showToDo(){
 }
 
 
-function deleteToDo(index){
-    myList.splice(index, 1);
-
-    showToDo();
-}
 
 function finishToDo(index){
 
     myList[index].done = !myList[index].done;
     showToDo();  
 }
+
+function editToggle(index){
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    toDoListContainer.classList.toggle("hide");
+    toolbar.classList.toggle("hide");
+
+    inputEdit.value = myList[index].text;
+
+    buttonEdit.addEventListener("click", (e) => {
+        e.preventDefault();
+    
+        myList[index].text = inputEdit.value;
+
+        editForm.classList.toggle("hide");
+        todoForm.classList.toggle("hide");
+        toDoListContainer.classList.toggle("hide");
+        toolbar.classList.toggle("hide");
+
+        showToDo();
+    })
+}
+
+
+
+function deleteToDo(index){
+    myList.splice(index, 1);
+
+    showToDo();
+}
+
 
 function localList(){
     const itemLocalList = localStorage.getItem('list');
